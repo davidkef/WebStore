35d3638bf466346f376847eeaa4b3b50ff97d9de
Ólafur Brynjar Jónsson <lafur Brynjar Jnsson>
2017-02-26 20:48:02 +0000

    test

---
+++ test/WebStoreSolution/WebStore/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            ViewBag.Message = "Your Product page.";
+
+            return View();
+        }
+        public ActionResult CreateNew()
+        {
+            ViewBag.Message = "Your Product page.";
+
+            return View();
+        }
+    }
+}
